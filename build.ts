84203f1709c50f06aa0e0d404c220e18fe72f6a0
0nepeop1e <engshun.lim@gmail.com>
2026-04-27 16:39:54 +0800

    fix: browser and server exports

--- build.ts
+++ build.ts
@@ -1,25 +1,65 @@
 import { rollup } from "rollup";
 import { dts } from "rollup-plugin-dts";
 
-await Bun.build({
+const reset = "\u001b[0m";
+
+const colors: Record<Bun.BuildOutput["logs"][number]["level"], string> = {
+	error: Bun.color("red", "ansi") ?? "",
+	warning: Bun.color("yellow", "ansi") ?? "",
+	info: Bun.color("blue", "ansi") ?? "",
+	debug: Bun.color("gray", "ansi") ?? "",
+	verbose: Bun.color("gray", "ansi") ?? "",
+};
+
+function log(output: Bun.BuildOutput) {
+	output.logs.forEach((log) => {
+		const color = colors[log.level];
+		console.log(`${color}[${log.level.toUpperCase()}] ${log.message}${reset}`);
+	});
+}
+
+console.log("Building...");
+await Bun.$`rm -rf dist`;
+
+console.time("build browser bundle");
+let out = await Bun.build({
 	entrypoints: ["src/index.ts"],
-	outdir: "dist",
+	outdir: "dist/browser",
 	sourcemap: "linked",
 	target: "browser",
 	format: "esm",
 	naming: { entry: "[name].mjs" },
 });
+console.timeEnd("build browser bundle");
+log(out);
 
-await Bun.build({
+console.time("build node esm bundle");
+out = await Bun.build({
 	entrypoints: ["src/index.ts"],
 	outdir: "dist",
 	sourcemap: "linked",
-	target: "browser",
+	target: "node",
+	format: "esm",
+	naming: { entry: "[name].mjs" },
+});
+console.timeEnd("build node esm bundle");
+log(out);
+
+console.time("build node cjs bundle");
+out = await Bun.build({
+	entrypoints: ["src/index.ts"],
+	outdir: "dist",
+	sourcemap: "linked",
+	target: "node",
 	format: "cjs",
 	naming: { entry: "[name].cjs" },
 });
+console.timeEnd("build node cjs bundle");
+log(out);
 
+console.time("build types");
 await rollup({
 	input: "src/index.ts",
 	plugins: [dts()],
-}).then((bundle) => bundle.write({ file: "dist/index.d.ts", format: "es", sourcemap: "inline" }));
+}).then((bundle) => bundle.write({ file: "dist/index.d.ts", format: "es", sourcemap: true }));
+console.timeEnd("build types");
