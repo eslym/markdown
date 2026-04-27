aaf376bf718c42d2fa45016b6f01543d3ef693f9
0nepeop1e <engshun.lim@gmail.com>
2026-04-27 17:23:00 +0800

    fix: node walker types

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { calculateRevisionHash } from "./revision" with { type: "macro" };
 import { Walker, Walker as WalkerClass } from "./walker";
 import type { MD } from "./types";
 
-export interface NodeWalker extends WalkerClass<
-	MD.NodeMap & { root: MD.Document } & { [key: string]: MD.Nodes }
-> {}
+type Prettify<T> = {
+	[K in keyof T]: T[K];
+} & {};
+
+export interface NodeWalker extends WalkerClass<Prettify<MD.NodeMap & { root: MD.Document }>> {}
 export const NodeWalker = Walker as {
 	new (): NodeWalker;
 };
